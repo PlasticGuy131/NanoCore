c6c320b966610acf78b5ef26e8eb21bc1ce118f2
PlasticGuy <daniel.myhill@virginmedia.com>
2024-02-22 20:48:08 +0000

    fix variable name

--- kernel.c
+++ kernel.c
@@ -71,7 +71,7 @@ void terminal_setcolour(uint8_t colour)
 
 void terminal_putentryat(char c, uint8_t colour, size_t x, size_t y)
 {
-    const size_t index = y * VGA_WIDTGH + x;
+    const size_t index = y * VGA_WIDTH + x;
     terminal_buffer[index] = vga_entry(c, colour);
 }
 

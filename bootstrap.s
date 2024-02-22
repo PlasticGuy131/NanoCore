698c4340f0dcefe1abf80ec3eeadc4ea2e370d59
PlasticGuy <daniel.myhill@virginmedia.com>
2024-02-22 19:25:04 +0000

    Added initial bootstrapper

---
+++ bootstrap.s
@@ -0,0 +1,48 @@
+/ * Multiboot header constants * /
+.set ALIGN,    1<<0
+.set MEMINFO,  1<<1
+.set FLAGS,    ALIGN | MEMINFO
+.set MAGIC,    0x1BADB002
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+/ * Create multiboot header * /
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+
+/ * Multiboot does not define a stack, and so this allocates a small one of 16 KiB * /
+.section .bss
+.align 16
+stack_bottom:
+.skip 16384
+stack_top:
+
+/ * _start is the entry points * /
+.section .text
+.global _start
+.type _start, @function
+_start:
+    / * bootloader has loaded into 32-bit protected mode on an x86 machine. Interrupts are disabled, * /
+    / * paging is diabled, and kernel has full control of the CPU. * /
+    
+    / * stack is setup by moving esp (top-of-stack pointer) to stack_top. This is done in assembly as C * /
+    / * cannot function without a stack. * /
+    mov $stack_top, %esp
+
+    / * Cruicial processor state should be initialised here before the full kernel starts. This means * /
+    / * floats, GDT, paging and some c++ features will not work untill they are implemented here. * /
+
+
+
+    / * Enter the high-level kernel * /
+    call kernel_main
+
+    / * Put the computer into an infinite loop. Clear interrupts, halt, then re-halt if it wakes up * /
+    cli
+    1:
+    hlt
+    jmp 1b
+
+/ * Set the size of the _start symbol to the current location ('.'), minus its start. * /
+.size _start, . - _start   

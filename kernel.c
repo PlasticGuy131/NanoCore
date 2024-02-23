0b7e5ad725603f9e0e5fc39d6f4ca9269b9fa967
PlasticGuy <daniel.myhill@virginmedia.com>
2024-02-23 00:10:21 +0000

    Added intro splash

--- kernel.c
+++ kernel.c
@@ -104,9 +104,23 @@ void terminal_writestring(const char* data)
     terminal_write(data, strlen(data));
 }
 
+void terminal_intro_splash()
+{
+    terminal_setcolour(vga_entry_colour(VGA_COLOUR_RED, VGA_COLOUR_BLACK));
+    terminal_writestring("NANO/CORE/NANO/CORE/\n")
+    terminal_setcolour(vga_entry_colour(VGA_COLOUR_GREEN, VGA_COLOUR_BLACK));
+    terminal_writestring("C/OREN/ANOC/OREN/ANO\n")
+    terminal_setcolour(vga_entry_colour(VGA_COLOUR_CYAN, VGA_COLOUR_BLACK));
+    terminal_writestring("NA/NOCO/RENA/NOCO/RE\n")
+    terminal_setcolour(vga_entry_colour(VGA_COLOUR_BLUE, VGA_COLOUR_BLACK));
+    terminal_writestring("COR/ENAN/OCOR/ENAN/O\n")
+    terminal_setcolour(vga_entry_colour(VGA_COLOUR_WHITE, VGA_COLOUR_BLACK));
+    terminal_writestring("NANO CORE\nv.N/A\nWelcome.\n")
+}
+
 void kernel_main(void)
 {
     terminal_initialize();
 
-    terminal_writestring("Hello World!\nTesting Newlines!");
+    terminal_intro_splash();
 }

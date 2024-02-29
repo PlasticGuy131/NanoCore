a27af85034f2349881a030aa47711078016c8251
PlasticGuy <daniel.myhill@virginmedia.com>
2024-02-29 11:46:25 +0000

    fix

--- kernel/arch/i386/terminal.c
+++ kernel/arch/i386/terminal.c
@@ -56,7 +56,7 @@ void terminal_initialize(void)
     terminal_writeint((int)multiboot_info);
     terminal_writestring("\n");
 
-    screen = (char*)multiboot_info->framebuffer_addr;
+    char* screen = (char*)multiboot_info->framebuffer_addr;
     for (int x = 1; x < 11; x++)
     {
         for (int y = 1; y < 11; y++)

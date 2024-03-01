565228790943a9adec08a0a1cacc66ac26b04b23
PlasticGuy <daniel.myhill@virginmedia.com>
2024-03-01 19:54:03 +0000

    hmm

--- kernel/arch/i386/terminal.c
+++ kernel/arch/i386/terminal.c
@@ -12,7 +12,7 @@ enum Display_Type
 {
     DISPLAY_SERIAL_ONLY = 0,
     DISPLAY_RGB = 1,
-    DISPLAY_VGA = 2;
+    DISPLAY_VGA = 2
 };
 
 enum Display_Type display_type;
@@ -33,79 +33,6 @@ static const size_t terminal_char_width = 9;
 uint16_t unicode[512];
 char* font_offset;
 
-static void terminal_vga_initialize(Multiboot_Info* multiboot_info)
-{
-    uint8_t terminal_colour = vga_entry_colour(COLOUR_LIGHT_GRAY, COLOUR_BLACK);
-    terminal_buffer = (uint16_t *)multiboot_info->framebuffer_addr;
-    for (size_t y = 0; y < VGA_HEIGHT; y++)
-    {
-        for (size_t x = 0; x < VGA_WIDTH; x++)
-        {
-            const size_t index = y * VGA_WIDTH + x;
-            terminal_buffer[index] = vga_entry(' ', terminal_colour);
-        }
-    }
-    terminal_height = VGA_HEIGHT;
-}
-
-static void terminal_rgb_initialize(Multiboot_Info* multiboot_info)
-{
-    PSF_Header* font = psf_get_header();
-    terminal_font_char_size = font->character_size;
-    font_offset = psf_setup_font(unicode);
-    screen_initialize(multiboot_info);
-
-    terminal_height = multiboot_info->framebuffer_height / terminal_font_char_size;
-    terminal_width = multiboot_info->framebuffer_width / terminal_char_width;
-}
-
-
-void terminal_initialize(void)
-{
-    if(multiboot_magic != MULTIBOOT_MAGIC)
-    {
-        terminal_writestring("ERROR: NOT LOADED WITH MULTIBOOT, PANIC!\n");
-        return;
-    }
-
-    Multiboot_Info* multiboot_info = (Multiboot_Info*)multiboot_info_start;
-    display_type = multiboot_info->framebuffer_type;
-
-    terminal_row = 0;
-    terminal_column = 0;
-
-    if (display_type == DISPLAY_RGB)
-    {
-        terminal_rgb_initialize(multiboot_info);
-        terminal_writestring("INFO: Terminal initialized in RGB mode.\n");
-        terminal_putcharat = &terminal_rgb_putcharat;
-        terminal_scroll = &terminal_rgb_scroll;
-    }
-    else if (display_type == DISPLAY_VGA)
-    {
-        terminal_vga_initialize(multiboot_info);
-        terminal_writestring("INFO: Terminal initialized in VGA mode.\n");
-        terminal_putcharat = &terminal_vga_putentryat;
-        terminal_scroll = &terminal_vga_scroll;
-    }
-    else
-    {
-        terminal_writestring("WARNING: Terminal graphics type not supported, all output will be serial.\n");
-        terminal_putcharat = NULL;
-        terminal_scroll = NULL;
-    }
-}
-
-static inline int terminal_xpixel(size_t x)
-{
-    return x * terminal_char_width;
-}
-
-static inline int terminal_ypixel(size_t y)
-{
-    return y * terminal_font_char_size;
-}
-
 static void terminal_vga_putentryat(char c, enum Colour fg, enum Colour bg, size_t x, size_t y)
 {
     const size_t index = y * VGA_WIDTH + x;
@@ -117,7 +44,7 @@ static void terminal_rgb_putcharat(char c, enum Colour fg, enum Colour bg, size_
     char* offset = font_offset;
     offset += unicode[c] * terminal_font_char_size;
     screen_putbitmap_bw(terminal_xpixel(x), terminal_ypixel(y), offset, 1, terminal_font_char_size, screen_rgb_name(fg), screen_rgb_name(bg));
-}   
+}
 
 static void terminal_vga_scroll()
 {
@@ -153,6 +80,82 @@ static void terminal_rgb_scroll()
     {
         terminal_putentryat(' ', terminal_fg_colour, terminal_bg_colour, x, terminal_height - 1);
     }
+}
+
+static void terminal_vga_initialize(Multiboot_Info* multiboot_info)
+{
+    uint8_t terminal_colour = vga_entry_colour(COLOUR_LIGHT_GRAY, COLOUR_BLACK);
+    terminal_buffer = (uint16_t *)multiboot_info->framebuffer_addr;
+    for (size_t y = 0; y < VGA_HEIGHT; y++)
+    {
+        for (size_t x = 0; x < VGA_WIDTH; x++)
+        {
+            const size_t index = y * VGA_WIDTH + x;
+            terminal_buffer[index] = vga_entry(' ', terminal_colour);
+        }
+    }
+
+    terminal_height = VGA_HEIGHT;
+    terminal_width = VGA_WIDTH;
+
+    terminal_putcharat = &terminal_vga_putentryat;
+    terminal_scroll = &terminal_vga_scroll;
+}
+
+static void terminal_rgb_initialize(Multiboot_Info* multiboot_info)
+{
+    PSF_Header* font = psf_get_header();
+    terminal_font_char_size = font->character_size;
+    font_offset = psf_setup_font(unicode);
+    screen_initialize(multiboot_info);
+
+    terminal_height = multiboot_info->framebuffer_height / terminal_font_char_size;
+    terminal_width = multiboot_info->framebuffer_width / terminal_char_width;
+
+    terminal_putcharat = &terminal_rgb_putcharat;
+    terminal_scroll = &terminal_rgb_scroll;
+}
+
+void terminal_initialize(void)
+{
+    if(multiboot_magic != MULTIBOOT_MAGIC)
+    {
+        terminal_writestring("ERROR: NOT LOADED WITH MULTIBOOT, PANIC!\n");
+        return;
+    }
+
+    Multiboot_Info* multiboot_info = (Multiboot_Info*)multiboot_info_start;
+    display_type = multiboot_info->framebuffer_type;
+
+    terminal_row = 0;
+    terminal_column = 0;
+
+    if (display_type == DISPLAY_RGB)
+    {
+        terminal_rgb_initialize(multiboot_info);
+        terminal_writestring("INFO: Terminal initialized in RGB mode.\n");
+    }
+    else if (display_type == DISPLAY_VGA)
+    {
+        terminal_vga_initialize(multiboot_info);
+        terminal_writestring("INFO: Terminal initialized in VGA mode.\n");
+    }
+    else
+    {
+        terminal_writestring("WARNING: Terminal graphics type not supported, all output will be serial.\n");
+        terminal_putcharat = NULL;
+        terminal_scroll = NULL;
+    }
+}
+
+static inline int terminal_xpixel(size_t x)
+{
+    return x * terminal_char_width;
+}
+
+static inline int terminal_ypixel(size_t y)
+{
+    return y * terminal_font_char_size;
 }
 
 void terminal_putchar(char c)

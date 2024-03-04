54f0dc85ec0903b49d663d2bf5097228b7a4fb6a
PlasticGuy <daniel.myhill@virginmedia.com>
2024-03-04 23:41:18 +0000

    fix

--- kernel/arch/i386/port/port.c
+++ kernel/arch/i386/port/port.c
@@ -3,8 +3,8 @@
 #include <port.h>
 
 inline void outb(uint16_t port, uint8_t val)  { __asm__ volatile("outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory"); }
-inline void outw(uint16_t port, uint16_t val) { __asm__ volatile("outw %b0, %w1" : : "a"(val), "Nd"(port) : "memory"); }
-inline void outl(uint16_t port, uint32_t val) { __asm__ volatile("outl %b0, %w1" : : "a"(val), "Nd"(port) : "memory"); }
+inline void outw(uint16_t port, uint16_t val) { __asm__ volatile("outw %w0, %w1" : : "a"(val), "Nd"(port) : "memory"); }
+inline void outl(uint16_t port, uint32_t val) { __asm__ volatile("outl %l0, %w1" : : "a"(val), "Nd"(port) : "memory"); }
 
 inline uint8_t inb(uint16_t port) 
 {
@@ -16,14 +16,14 @@ inline uint8_t inb(uint16_t port)
 inline uint16_t inw(uint16_t port)
 {
     uint16_t ret;
-    __asm__ volatile("inw %w1, %b0" : "=a"(ret) : "Nd"(port) : "memory");
+    __asm__ volatile("inw %w1, %w0" : "=a"(ret) : "Nd"(port) : "memory");
     return ret;
 }
 
 inline uint32_t inl(uint16_t port)
 {
     uint32_t ret;
-    __asm__ volatile("inl %w1, %b0" : "=a"(ret) : "Nd"(port) : "memory");
+    __asm__ volatile("inl %w1, %l0" : "=a"(ret) : "Nd"(port) : "memory");
     return ret;
 }
 

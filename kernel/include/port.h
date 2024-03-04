a4efc009d596fb1a31b483230d5266bf9f3cc9ee
PlasticGuy <daniel.myhill@virginmedia.com>
2024-03-04 23:35:44 +0000

    hmm

--- kernel/include/port.h
+++ kernel/include/port.h
@@ -5,13 +5,13 @@
 
 static const uint32_t BDA_START = 0x0400;
 
-inline void outb(uint16_t port, uint8_t val);
-inline void outw(uint16_t port, uint16_t val);
-inline void outl(uint16_t port, uint32_t val);
+void outb(uint16_t port, uint8_t val);
+void outw(uint16_t port, uint16_t val);
+void outl(uint16_t port, uint32_t val);
 
-inline uint8_t inb(uint16_t port);
-inline uint16_t inw(uint16_t port);
-inline uint32_t inl(uint16_t port);
+uint8_t inb(uint16_t port);
+uint16_t inw(uint16_t port);
+uint32_t inl(uint16_t port);
 
 uint32_t port_get_com1();
 

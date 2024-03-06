7fb17decda214fc9e963a9a3a4d3eb365b71a6d3
PlasticGuy <daniel.myhill@virginmedia.com>
2024-03-06 18:42:59 +0000

    f

--- libc/string/common/memcpy.c
+++ libc/string/common/memcpy.c
@@ -2,7 +2,7 @@
 
 #include "string.h"
 
-void* memcpy(restrict void* dstptr, const restrict void* srcptr, size_t size)
+void* memcpy(void* restrict dstptr, const void* restrict srcptr, size_t size)
 {
     unsigned char* dst = (unsigned char*)dstptr;
     const unsigned char* src = (const unsigned char*)srcptr;

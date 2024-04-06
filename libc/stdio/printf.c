b093b86b13813c94d6780f4a6fce28f5da92c540
PlasticGuy <daniel.myhill@virginmedia.com>
2024-04-06 12:35:13 +0100

    f

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -118,9 +118,6 @@ static int print_float(double f, int (*put)(int), size_t written, unsigned max, 
         test_f *= 10.0;
         unsigned test_c = test_f;
         test_f -= test_c;
-        put('\n');
-        put(test_c + '0');
-        put('\n');
         if (test_c != 0)
         {
             stop = false;

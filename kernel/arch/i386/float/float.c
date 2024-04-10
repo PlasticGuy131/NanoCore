2f8a27d883c2300b8516100575a69d6e6b38a897
PlasticGuy <daniel.myhill@virginmedia.com>
2024-04-10 21:50:35 +0100

    test

--- kernel/arch/i386/float/float.c
+++ kernel/arch/i386/float/float.c
@@ -8,7 +8,7 @@ extern uint32_t fpu_test;
 
 int float_initialize()
 {
-    if (fpu_test == 0)
+    if (fpu_test != 0)
     {   
         terminal_col_error();
         printf("ERROR: FLOAT INITIALIZATION FAILED\n");

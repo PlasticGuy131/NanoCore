4e58a0694c0756cf85f775e37a8fd5d487ea7c3c
PlasticGuy <daniel.myhill@virginmedia.com>
2024-04-11 02:56:27 +0100

    hmmm..

--- kernel/arch/i386/memory/memory.c
+++ kernel/arch/i386/memory/memory.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 
@@ -7,11 +8,21 @@
 extern uint32_t _heap_start;
 extern uint32_t _heap_end;
 
-void memory_initialize(Multiboot_Info* multiboot_info)
+typedef uint16_t bool16;
+
+struct block_header
+{
+    uint16_t size;
+    bool16 allocated;
+}
+
+void memory_initialize()
 {
     printf("Heap: %p - %p\n", _heap_start, _heap_end);
-    uint32_t* heap_start = (uint32_t*)(uintptr_t)_heap_start;
-    uint32_t heap_width = _heap_end - _heap_start;
-    *heap_start = heap_width;
+    struct block_header* header = (struct block_header*)(uintptr_t)_heap_start;
+    uint16_t heap_width = _heap_end - _heap_start;
+    struct block_header initial_header;
+    initial_header.size = heap_width;
+    initial_header.allocated = false;
     printf("Width: %p\n", heap_width);
 }

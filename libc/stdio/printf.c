62e68bc4b3cd5aa810b62d2ce9a4d1d99f8fde2f
PlasticGuy <daniel.myhill@virginmedia.com>
2024-06-27 20:16:12 +0100

    huh?

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -152,7 +152,7 @@ static int print_float(double f, int (*put)(int), size_t written, unsigned max, 
     }
     written += l;
 
-    if (truncate)
+    if (0)
     {
         offset = dp;
         while (offset > 0)

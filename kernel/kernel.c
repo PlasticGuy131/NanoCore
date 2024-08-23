9379173b538925364028747fb42c010b3d656939
PlasticGuy <daniel.myhill@virginmedia.com>
2024-08-23 01:32:00 +0100

    debug ig

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -88,36 +88,43 @@ void kernel_main(void)
 
     memory_visualise();
 
-    void* m = malloc(6);
-    memory_visualise();
-
-    free(m);
-    memory_visualise();
-
     printf("testing printf...\n");
     int o = 17;
     printf("OCTAL: %o\n", o);
+    memory_visualise();
     printf("OCTAL: %#o\n", o);
+    memory_visualise();
     double f = 5.000001;
     printf("FLOAT G: %g\n", f);
+    memory_visualise();
     printf("FLOAT F: %f\n", f);
+    memory_visualise();
     printf("\n");
     printf("FLOAT G: %#g\n", f);
+    memory_visualise();
     printf("FLOAT F: %#f\n", f);
+    memory_visualise();
     printf("HEX: %#x\n", 12);
+    memory_visualise();
     printf("HEX: %X\n", 12);
+    memory_visualise();
     printf("\n");
     f = 500000000;
     printf("FLOAT g: %g\n", f);
+    memory_visualise();
     printf("FLOAT e: %e\n", f);
+    memory_visualise();
     printf("\n");
     //printf("FLOAT G: %#G\n", f);
     printf("FLOAT E: %#E\n", f);
+    memory_visualise();
     printf("\n");
     f = 5.00001;
     printf("FLOAT g: %g\n", f);
+    memory_visualise();
     f = 50.00001;
     printf("FLOAT g: %g\n", f);
+    memory_visualise();
 
     printf("\nMEMORY USAGE: %i/%i\n", memory_usage(), memory_max());
     memory_visualise();

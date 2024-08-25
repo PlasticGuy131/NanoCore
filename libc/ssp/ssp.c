16fc39dc1811cafaace3abd9cb79327e781ca212
PlasticGuy <daniel.myhill@virginmedia.com>
2024-08-25 14:37:21 +0100

    stack smash

---
+++ libc/ssp/ssp.c
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+#if UINT32_MAX == UINTPTR_MAX
+#define STACK_CHK_GUARD 0x55ebc119
+#else
+#define STACK_CHK_GUARD 0x4a3809b5bc9e66d2
+#endif
+
+#ifdef __is_libk
+#include <kernel.h>
+#endif
+
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+__attribute__((noreturn)) void __stack_chk_fail(void)
+{
+#ifdef __is_libk
+    kernel_panic("Stack Smash Detected.");
+#else
+    //TODO: System calls
+#endif
+}

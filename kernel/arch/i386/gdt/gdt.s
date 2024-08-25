4629055bf1d9a3e22ee20c00ecc5b63db515b99c
PlasticGuy <daniel.myhill@virginmedia.com>
2024-08-25 18:39:35 +0100

    hmm

--- kernel/arch/i386/gdt/gdt.s
+++ kernel/arch/i386/gdt/gdt.s
@@ -4,9 +4,12 @@ gdtr:
 
 .globl set_gdt
 set_gdt:
-    mov $_gdt_start, %eax
+    mov $0, %eax
+    mov %ds, %ax
+    shl $4, %eax
+    add _gdt_start
     mov %eax, gdtr+2
-    mov $_gdt_end, %eax
-    sub $_gdt_start, %eax
+    mov _gdt_end, %eax
+    sub _gdt_start, %eax
     mov %eax, gdtr
-    lgdt gdtr
+    lgdt $gdtr

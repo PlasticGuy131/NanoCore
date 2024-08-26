2ba073157b461549672dd9e0138e063091889d1e
PlasticGuy <daniel.myhill@virginmedia.com>
2024-08-27 00:12:40 +0100

    hmm

--- kernel/common/interrupt/isr_wrappers.s
+++ kernel/common/interrupt/isr_wrappers.s
@@ -1,9 +1,7 @@
 .globl interrupt_panic_wpr
 interrupt_panic_wpr:
-    hlt
+    pushal
+    cld
+    call interrupt_panic
+    popal
     ret
-    //pushal
-    //cld
-    //call interrupt_panic
-    //popal
-    //ret

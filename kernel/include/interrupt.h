85fac0a9a5a4c5065fe2c725b4eab5b81f570027
PlasticGuy <daniel.myhill@virginmedia.com>
2024-08-28 23:04:56 +0100

    f

--- kernel/include/interrupt.h
+++ kernel/include/interrupt.h
@@ -1,6 +1,8 @@
 #ifndef INTERRUPT_H_INCLUDED
 #define INTERRUPT_H_INCLUDED 1
 
+#include <stdint.h>
+
 /// @brief Initialises interrupts
 /// @return Status Code: 0 OK, 1 ERROR
 int interrupt_initialize();

104e11d45e6fd992a067c4a75bc5f463e928b386
PlasticGuy <daniel.myhill@virginmedia.com>
2024-08-29 00:24:42 +0100

    hmm

--- kernel/include/clock.h
+++ kernel/include/clock.h
@@ -2,10 +2,10 @@
 #define CLOCK_H_INCLUDED 1
 
 /// @brief Increments the clock counter
-void clock_increment() { clock++; }
+void clock_increment();
 
 /// @brief Returns the value of the clock counter
 /// @return Current value of the clock
-int clock_time() { return clock; }
+int clock_time();
 
 #endif

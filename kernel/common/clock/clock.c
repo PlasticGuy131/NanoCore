753be509fda63367c7c44fdd30f20095ef42652b
PlasticGuy <daniel.myhill@virginmedia.com>
2024-08-29 15:28:53 +0100

    hmm

--- kernel/common/clock/clock.c
+++ kernel/common/clock/clock.c
@@ -1,6 +1,12 @@
+#include <stdio.h>
+
 #include <clock.h>
 
 static int clock = 0;
 
-void clock_increment() { clock++; }
+void clock_increment()
+{
+    printf(".");
+    clock++;
+}
 int clock_count() { return clock; }

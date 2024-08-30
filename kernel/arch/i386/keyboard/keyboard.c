537eaab6621c41ca66b8b6e581c859172a3e530e
PlasticGuy <daniel.myhill@virginmedia.com>
2024-08-30 18:07:51 +0100

    hmm

--- kernel/arch/i386/keyboard/keyboard.c
+++ kernel/arch/i386/keyboard/keyboard.c
@@ -11,12 +11,16 @@ static const int scancode_alpha_codes[26] = {0x1E, 0x30, 0x2E, 0x20, 0x12, 0x21,
                                              0x31, 0x18, 0x19, 0x10, 0x13, 0x1F, 0x14, 0x16, 0x2F, 0x11, 0x2D, 0x15, 0x2C};
 
 static const int scancode_symbol_codes[15] = {0x0C, 0x0D, 0x1A, 0x1B, 0x27,
-                                              0x28, 0x29, 0x2B, 0x33, 0x34,
+                                              0x28, 0x29, 0x56, 0x33, 0x34,
                                               0x35, 0x37, 0x4A, 0x4E, 0x53};
 
 static const char symbol_code_chars[15] = {'-', '=', '[', ']', ';',
                                            '\'', '`', '\\', ',', '.',
-                                           '/', '*', '-', '+', '.'};
+                                           '/', '*', '-', '+', '.' };
+
+static const int scancode_extra_codes[4] = { 0x0E, 0x0F, 0x1C, 0x39 };
+
+static const char extra_code_chars[4] = {'\b', '\t', '\n', ' '};
 
 static unsigned scancode_to_code(int scancode)
 {
@@ -28,7 +32,12 @@ static unsigned scancode_to_code(int scancode)
         if (scancode == scancode_symbol_codes[i]) { return i + 37; }
         else if (scancode < scancode_symbol_codes[i]) { break; }
     }
-    
+    for (int i = 0; i < 4; i++)
+    {
+        if (scancode == scancode_extra_codes[i]) { return i + 52; }
+        else if (scancode < scancode_extra_codes[i]) { break; }
+    }
+
     return 0;
 }
 
@@ -40,7 +49,8 @@ int keyboard_char_code(char ascii)
     if (isdigit(ascii)) { return ascii - '0' + 1; }
     if (isupper(ascii)) { return ascii - 54; }
     for (int i = 0; i < 15; i++) { if (ascii == symbol_code_chars[i]) { return i + 37; } }
-
+    for (int i = 0; i < 4; i++) { if (ascii == scancode_extra_codes[i]) { return i + 52; } }
+    
     return 0;
 }
 
@@ -50,13 +60,13 @@ char keyboard_keypress_char(Keypress keypress)
     if (keypress.code <= 10) { return keypress.code - 1 + '0'; }
     if (keypress.code <= 36) { return keypress.code + 54; }
     if (keypress.code <= 51) { return symbol_code_chars[keypress.code - 37]; }
+    if (keypress.code <= 55) { return scancode_extra_codes[keypress.code - 37]; }
     return '\0';
 }
 
 void keyboard_read_key()
 {
     int scancode = inb(PS2_DATA);
-    printf("Escape code: %#.2x\n", scancode);
     Keypress keypress;
     keypress.flags = 0;
     if (!(scancode & 0x80)) { keypress.flags |= KEY_FLAG_PRESSED; }
